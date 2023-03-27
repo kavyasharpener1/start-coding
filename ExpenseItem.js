f4303c4ae5641122f3c4583d4f0b8cd34399f81a
kavyasharpener1 <naikkavya201@gmail.com>
2023-03-27 18:40:15 +0530

    FrontendDevelopment

---
+++ ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>Petrol 110</div>
+            <div>
+                <h2>Food 150</h2>
+                <div>Hotel 500</div>
+            </div>
+        </div>
+    );
+}
+export default ExpenseItem;
